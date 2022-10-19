2f24d0006d8a85255cbc209ed71ce73392145f09
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2022-10-19 18:02:36 +0100

    Save commit

---
+++ app/src/main/java/uk/ac/chester/kotlinhelp/MainActivity.kt
@@ -0,0 +1,106 @@
+package uk.ac.chester.kotlinhelp
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import uk.ac.chester.kotlinhelp.databinding.ActivityMainBinding
+import java.net.URLEncoder
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    var prevUrl: String? = ""
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        toggleButtonsState(false)
+
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+
+        prevUrl = sharedPref.getString("prevUrl", "")
+
+        binding.previousButton.visibility = View.VISIBLE
+
+
+
+
+
+        binding.etSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                toggleButtonsState(binding.etSearch.text.length > 1)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        })
+
+
+        //initialising buttons
+        binding.googleButton.setOnClickListener {
+            loadWebActivity("https://google.co.uk/search?q=$encodedSearchTerm")
+        }
+        binding.stackOverflowButton.setOnClickListener {
+            loadWebActivity("https://stackoverflow.com/search?q=$encodedSearchTerm")
+        }
+        binding.kotlinDocsButton.setOnClickListener {
+            loadWebActivity("https://kotlinlang.org/search?q=$encodedSearchTerm&p=0")
+        }
+        binding.androidDevButton.setOnClickListener {
+            loadWebActivity("https://developer.android.com/s/?q=$encodedSearchTerm")
+        }
+        binding.previousButton.setOnClickListener {
+            loadWebActivity(prevUrl ?: "")
+        }
+
+
+    }
+
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val url = result.data?.getStringExtra("url")
+            if (url != null) {
+                prevUrl = url
+                binding.previousButton.visibility = View.VISIBLE
+            } else {
+                binding.previousButton.visibility = View.INVISIBLE
+            }
+        }
+
+    private fun loadWebActivity(url: String) {
+        val intent = Intent(this, WebSearchActivity::class.java)
+        intent.putExtra("url", url)
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putString("prevUrl", url)
+            apply()
+        }
+        resultLauncher.launch(intent)
+    }
+
+    private fun toggleButtonsState(enabled: Boolean) {
+        binding.googleButton.isEnabled = enabled
+        binding.kotlinDocsButton.isEnabled = enabled
+        binding.stackOverflowButton.isEnabled = enabled
+        binding.androidDevButton.isEnabled = enabled
+    }
+
+
+    private val encodedSearchTerm: String
+        get() = URLEncoder.encode(binding.etSearch.text.toString())
+}
---
+++ app/src/main/java/uk/ac/chester/kotlinhelp/WebSearchActivity.kt
@@ -0,0 +1,46 @@
+package uk.ac.chester.kotlinhelp
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.webkit.WebViewClient
+import uk.ac.chester.kotlinhelp.databinding.ActivityWebSearchBinding
+
+class WebSearchActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityWebSearchBinding
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWebSearchBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        var url = savedInstanceState?.getString("url") ?: intent.getStringExtra("url")
+
+
+        binding.webView.webViewClient = WebViewClient() //prevents opening in browser app
+        binding.webView.settings.javaScriptEnabled = true //required for kotlin and android sites
+        binding.webView.loadUrl(url!!)
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("url", binding.webView.url)
+    }
+
+
+    override fun onBackPressed() {
+        val intent = Intent(this, MainActivity::class.java).putExtra("url", binding.webView.url)
+        setResult(Activity.RESULT_OK, intent)
+        finish()
+        super.onBackPressed()
+    }
+}
